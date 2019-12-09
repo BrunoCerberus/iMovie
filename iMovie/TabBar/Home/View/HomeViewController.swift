ad239dbc71730eeee8411fd14c6e2c1a1944374d
Bruno Lopes de Mello <tqi_bmello@uolinc.com>
2019-12-09 10:37:13 -0300

    sections for the home screen defined

--- iMovie/TabBar/Home/View/HomeViewController.swift
+++ iMovie/TabBar/Home/View/HomeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class HomeViewController: BaseViewController {
     
+    @IBOutlet weak var homeCollection: UICollectionView!
+    
     var viewModel: HomeViewModel!
     
     init(viewModel: HomeViewModel) {
@@ -18,13 +20,22 @@ class HomeViewController: BaseViewController {
         title = "Home"
     }
     
+    enum HomeSections: Int {
+        case nowPlaying
+        case topRated
+        case popular
+        
+        static var count: Int { return 3 }
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.homeCollection.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,5 +47,32 @@ class HomeViewController: BaseViewController {
     override func viewWillDisappear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: false)
     }
+}
 
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 50, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
 }
